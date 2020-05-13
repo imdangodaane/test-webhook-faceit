f48e5cff23cec602888d88ed6d69a58f788f7aa4
Qui Nguyen <imdangodaane@gmail.com>
2020-05-13 15:57:13 +0700

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+import * as express from 'express';
+
+const app = express();
+
+app.all('/catch/match', (req, res) => {
+  console.log(res);
+});
+
+app.listen(4002, (err) => {
+  console.log(`[${new Date().toLocaleString()}]\tServer started and listen on PORT: 4002`);
+});
